8da806a00bc6efec9fedaf2c88b1a28cbe1f87c7
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-05-26 08:17:12 +0200

    Added code

--- src/core.cpp
+++ src/core.cpp
@@ -1,6 +1,6 @@
 #define US_TICKS 48
 #define UCC 2940
-#define U_ANALOG_MAX 45000.0f
+#define U_ANALOG_MAX 43000.0f
 
 extern "C" {
 #include "stm32f0xx_conf.h"
@@ -14,6 +14,96 @@ void delay_us(uint32_t us);
 #define LCD_DB0 (GPIO_Pin_3)
 #define LCD_EN (GPIO_Pin_5)
 #define LCD_RS (GPIO_Pin_6)
+
+// the 8 arrays that form each segment of the custom numbers
+unsigned char LT[8] = 
+{
+	0b00111,
+	0b01111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111
+};
+unsigned char UB[8] =
+{
+	0b11111,
+	0b11111,
+	0b11111,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000
+};
+unsigned char RT[8] =
+{
+	0b11100,
+	0b11110,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111
+};
+unsigned char LL[8] =
+{
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b01111,
+	0b00111
+};
+unsigned char LB[8] =
+{
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b11111,
+	0b11111,
+	0b11111
+};
+unsigned char LR[8] =
+{
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11110,
+	0b11100
+};
+unsigned char MB[8] =
+{
+	0b11111,
+	0b11111,
+	0b11111,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b11111,
+	0b11111
+};
+unsigned char block[8] =
+{
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111
+};
 
 class LCDDisplay
 {
@@ -75,6 +165,11 @@ public:
 		}
 	}
 
+	void writeChar(uint8_t ch)
+	{
+		displayWrite(CharactersType, ch);
+	}
+
 	void writeNumber(uint32_t number)
 	{
 		static const uint32_t base[] = { 1000000000, 100000000, 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1 };
@@ -87,10 +182,149 @@ public:
 		writeString(numString);
 	}
 
+	void writeTemperature(int number)
+	{
+		static const uint32_t base[] = { 100, 10, 1 };
+		for (int pos = 0; pos < 3; ++pos) {
+			uint8_t digit = number / base[pos];
+			number -= digit * base[pos];
+			writeBigNumberAt(digit, pos < 2 ? pos * 4 : (pos * 4 + 2));
+		}
+		setCursor(8, 1);
+		writeChar(4);
+		setCursor(14, 0);
+		writeChar('o');
+	}
+
 	void home()
 	{
 		displayWrite(CommandType, 0x02);
 		delay_us(1640);
+	}
+
+	void setCursor(int col, int row)
+	{
+		displayWrite(CommandType, 0x80 | col | (row == 0 ? 0 : 0x40));
+		delay_us(1640);
+	}
+
+	void createChar(uint8_t location, uint8_t *charmap)
+	{
+		displayWrite(CommandType, 0x40 | (location << 3));
+		for (int i = 0; i < 8; ++i) {
+			displayWrite(CharactersType, charmap[i]);
+		}
+		delay_us(1640);
+	}
+
+	void writeBigNumberAt(uint8_t number, int pos)
+	{
+		setCursor(pos, 0);
+		switch (number) {
+			case 0:
+				writeChar(0);
+				writeChar(1);
+				writeChar(2);
+				break;
+			case 1:
+				writeChar(1);
+				writeChar(2);
+				writeChar(' ');
+				break;
+			case 2:
+				writeChar(6);
+				writeChar(6);
+				writeChar(2);
+				break;
+			case 3:
+				writeChar(6);
+				writeChar(6);
+				writeChar(2);
+				break;
+			case 4:
+				writeChar(3);
+				writeChar(4);
+				writeChar(7);
+				break;
+			case 5:
+				writeChar(3);
+				writeChar(6);
+				writeChar(6);
+				break;
+			case 6:
+				writeChar(0);
+				writeChar(6);
+				writeChar(6);
+				break;
+			case 7:
+				writeChar(1);
+				writeChar(1);
+				writeChar(2);
+				break;
+			case 8:
+				writeChar(0);
+				writeChar(6);
+				writeChar(2);
+				break;
+			case 9:
+				writeChar(0);
+				writeChar(6);
+				writeChar(2);
+				break;
+		}
+		setCursor(pos, 1);
+		switch (number) {
+			case 0:
+				writeChar(3);
+				writeChar(4);
+				writeChar(5);
+				break;
+			case 1:
+				writeChar(4);
+				writeChar(7);
+				writeChar(4);
+				break;
+			case 2:
+				writeChar(3);
+				writeChar(4);
+				writeChar(4);
+				break;
+			case 3:
+				writeChar(4);
+				writeChar(4);
+				writeChar(5);
+				break;
+			case 4:
+				writeChar(' ');
+				writeChar(' ');
+				writeChar(7);
+				break;
+			case 5:
+				writeChar(4);
+				writeChar(4);
+				writeChar(5);
+				break;
+			case 6:
+				writeChar(3);
+				writeChar(4);
+				writeChar(5);
+				break;
+			case 7:
+				writeChar(' ');
+				writeChar(' ');
+				writeChar(7);
+				break;
+			case 8:
+				writeChar(3);
+				writeChar(4);
+				writeChar(5);
+				break;
+			case 9:
+				writeChar(' ');
+				writeChar(' ');
+				writeChar(7);
+				break;
+		}
 	}
 
 private:
@@ -170,15 +404,31 @@ void mainprog(void)
 
 	LCDDisplay display;
 	display.initialize();
+	display.createChar(0, LT);
+	display.createChar(1, UB);
+	display.createChar(2, RT);
+	display.createChar(3, LL);
+	display.createChar(4, LB);
+	display.createChar(5, LR);
+	display.createChar(6, MB);
+	display.createChar(7, block);
+
+	display.setCursor(0, 0);
+	display.writeTemperature(123);
 
 	while (1) {
-		display.home();
-		i = ADC_GetConversionValue(ADC1);
-		float x = float(i) / U_ANALOG_MAX;
-		// zdroj: http://www.mosaic-industries.com/embedded-systems/microcontroller-projects/temperature-measurement/ntc-thermistors/example-code-equations
-		float t = 25.0f + (x*(-99.296f + x*(24.624f + x*147.26f))) / (1.0f + x*(0.0195f + x*2.8454f));
-		display.writeNumber(int(t * 10));
-		delay_us(40000);
+		//display.setCursor(0, 0);
+		int t_sum = 0;
+		for (int j = 0; j < 64; ++j) {
+			i = ADC_GetConversionValue(ADC1);
+			float x = float(i) / U_ANALOG_MAX;
+			// zdroj: http://www.mosaic-industries.com/embedded-systems/microcontroller-projects/temperature-measurement/ntc-thermistors/example-code-equations
+			float t = 25.0f + (x*(-99.296f + x*(24.624f + x*147.26f))) / (1.0f + x*(0.0195f + x*2.8454f));
+			t_sum += int(t * 10);
+			delay_us(1000);
+		}
+		//display.writeNumber(int(t * 10));
+		display.writeTemperature(int(t_sum / 64));
 		GPIOC->ODR ^= (1 << 9);
 	}
 }
